87b46f99c42c386d46344d4b357c36b7bb9511a7
Ramanujen <RAMANUJAN@TUTAMAIL.COM>
2025-08-16 22:08:59 +0300

    test2

--- src/sum.test.js
+++ src/sum.test.js
@@ -7,4 +7,9 @@ describe("Test function sum", () => {
     it("return 5", () => {
         expect(sum(3,2)).toEqual(5);
     });
+    it("return 3", () => {
+        expect(sum(2,1)).toEqual(3);
+    });
+
+    
 });
